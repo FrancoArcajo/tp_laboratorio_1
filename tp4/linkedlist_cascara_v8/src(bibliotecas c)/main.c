a820f4bd07bda01b7ce0342987de0ca8708391b0
FrancoArcajo <francooarcajo@gmail.com>
2020-11-23 16:57:14 -0300

    tp finalizado

--- tp4/linkedlist_cascara_v8/src(bibliotecas c)/main.c
+++ tp4/linkedlist_cascara_v8/src(bibliotecas c)/main.c
@@ -168,25 +168,25 @@ int main()
 
 /*int main(void)
 {
-        startTesting(1);  // ll_newLinkedList SI
-        startTesting(2);  // ll_len SI
-        startTesting(3);  // getNode - test_getNode (no se usa)
-        startTesting(4);  // addNode - test_addNode (no se usa)
-        startTesting(5);  // ll_add SI
-        startTesting(6);  // ll_get SI
-        startTesting(7);  // ll_set SI
-        startTesting(8);  // ll_remove SI
-        startTesting(9);  // ll_clear SI
-        startTesting(10); // ll_deleteLinkedList SI
+        startTesting(1);  // ll_newLinkedList
+        startTesting(2);  // ll_len
+        startTesting(3);  // getNode - test_getNode
+        startTesting(4);  // addNode - test_addNode
+        startTesting(5);  // ll_add
+        startTesting(6);  // ll_get
+        startTesting(7);  // ll_set
+        startTesting(8);  // ll_remove
+        startTesting(9);  // ll_clear
+        startTesting(10); // ll_deleteLinkedList
         startTesting(11); // ll_indexOf
-        startTesting(12); // ll_isEmpty SI
-        startTesting(13); // ll_push SI
-        startTesting(14); // ll_pop SI
-        startTesting(15); // ll_contains SI
-        startTesting(16); // ll_containsAll SI
-        startTesting(17); // ll_subList SI
-        startTesting(18); // ll_clone SI
-        startTesting(19); // ll_sort SI
+        startTesting(12); // ll_isEmpty
+        startTesting(13); // ll_push
+        startTesting(14); // ll_pop
+        startTesting(15); // ll_contains
+        startTesting(16); // ll_containsAll
+        startTesting(17); // ll_subList
+        startTesting(18); // ll_clone
+        startTesting(19); // ll_sort
 
     return 0;
 }*/
